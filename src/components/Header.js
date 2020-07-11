cc5030471208283bd8d96d5afea36324aeef2371
karan0797 <karanjeet.anand@proptiger.com>
2020-07-11 16:50:16 +0530

    Deploy

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+// Highly customisable and generic component if header, which accepts tile value as props
+
+import {
+  AppBar, Toolbar, Typography, makeStyles,
+} from '@material-ui/core';
+
+import logo from '../../public/Images/inspect/Front-End/Page 1/logo.png';
+
+const useStyles = makeStyles(() => ({
+  appbarWrapperStyle: {
+    height: 55,
+  },
+  appbarStyle: {
+    '&.MuiAppBar-root': {
+      justifyContent: 'space-between',
+      backgroundColor: '#1F2640',
+    },
+  },
+  imgContainer: {
+    width: '10.2%',
+    paddingLeft: '12.5%',
+    paddingTop: '2%',
+    paddingBottom: '2%',
+  },
+  titleStyle: {
+    marginLeft: 16,
+    '&.MuiTypography-root': {
+      borderLeft: 'solid 1px #979797',
+      paddingLeft: 15,
+      fontSize: 16,
+      color: '#383838',
+    },
+  },
+}));
+
+const Header = ({ title = '' }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.appbarWrapperStyle}>
+      <AppBar className={classes.appbarStyle}>
+        <Toolbar>
+          <div className={classes.imgContainer}>
+            <img src={logo} alt="Logo" />
+          </div>
+          <Typography variant="h6" className={classes.titleStyle}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/utils/constants/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+  upcomingStatus: 'upcoming',
+  liveStatus: 'live',
+  pastStatus: 'past',
+  tableHeadings: ['Date', 'Campaigns', 'View', 'Actions'],
+  topGamesPath: 'Images/inspect/Front-End/Page 1/Dashboard',
+  pricingIconPath: 'Images/inspect/Front-End/Page 1/Dashboard/Row Copy 2-Row/Group 4/Price.png',
+  csvIconPath: 'Images/inspect/Front-End/Page 1//Dashboard/Row Copy 2-Row/Group 3/file.png',
+  reportIconPath: 'Images/inspect/Front-End/Page 1/Dashboard/Row Copy 2-Row/Group 2/statistics-report.png',
+  timeDurations: ['1 Week - 1 Month', '6 Months', '1 Year'],
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+};
+
+export default constants;
